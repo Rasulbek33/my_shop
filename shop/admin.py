c411c4fa54ed3fb8fb40c63412b933bcf4981214
Rasulbek <rasulbekxoldorov8@gmail.com>
2023-10-09 20:58:25 +0500

    add models, add urls,add views, add admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category,Product
 
-# Register your models here.
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    list_display = ['name','slug']
+
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    list_display = ['name','slug','price',
+                    'available','created','updated']
+    list_filter = ['available','created','updated']
+    list_editable = ['price','available']
+    
